83769a8b8e1fd643b1a430c084c20bc04ba8389d
iugbhjvhgvh <justin.guo01@gmail.com>
2018-11-26 17:16:39 -0500

    First draft of move method in autonomous, testing it in CraterAuto Left

--- TeamCode/src/main/java/org/firstinspires/ftc/AutonomousBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/AutonomousBase.java
@@ -6,8 +6,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public abstract class AutonomousBase extends OpModeBase {
 
     private HardwareK9bot robot = new HardwareK9bot();
-
     public MovementHelper helper;
+
     private boolean red;
 
     public AutonomousBase(boolean red) {
@@ -31,6 +31,49 @@ public abstract class AutonomousBase extends OpModeBase {
     }
 
     public abstract void runOpModeImpl();
+
+    public void move(double distance, double angle, double power)
+    {
+        power = Math.abs(power);
+        //set x,y coords
+        double lateral = Math.cos(angle);
+        double forward = -Math.sin(angle);
+
+        double leftDrive, leftBack, rightDrive, rightBack;
+
+        //mecanum wheel function
+        leftDrive = forward + lateral;
+        leftBack = forward - lateral;
+        rightDrive = forward - lateral;
+        rightBack = forward + lateral;
+
+        //track ticks of left wheel for distance
+        double ticks = 0;
+        if(leftDrive != 0) {
+            robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            while (opModeIsActive() && ticks < 537.6/Math.PI * distance * Math.sqrt(2)) {
+                ticks = robot.leftDrive.getCurrentPosition();
+                robot.leftDrive.setPower(leftDrive * power);
+                robot.leftBack.setPower(leftBack * power);
+                robot.rightDrive.setPower(rightDrive * power);
+                robot.rightBack.setPower(rightBack * power);
+            }
+        }
+        else //track ticks of right wheel for distance
+        {
+            robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            while (opModeIsActive() && ticks < 537.6/Math.PI * distance * Math.sqrt(2)) {
+                ticks = robot.rightDrive.getCurrentPosition();
+                robot.leftDrive.setPower(leftDrive * power);
+                robot.leftBack.setPower(leftBack * power);
+                robot.rightDrive.setPower(rightDrive * power);
+                robot.rightBack.setPower(rightBack * power);
+            }
+        }
+
+    }
 
     public void deployBot()
     {
