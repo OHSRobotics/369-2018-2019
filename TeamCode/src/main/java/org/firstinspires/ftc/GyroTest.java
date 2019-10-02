315b9ed838a9c8833dca3519775e982cdf4ee2ac
Zhentarim-Cloak <55816863+Zhentarim-Cloak@users.noreply.github.com>
2019-10-02 18:54:23 -0400

    Update GyroTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/GyroTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/GyroTest.java
@@ -2,13 +2,13 @@ package org.firstinspires.ftc;
 
 public class GyroTest{/*
 
-
+//difining class
     //public class Gyro extends OpModeBase {/*
 
         public void gyroTurnTo(double heading) {
             HardwareK9bot robot = new HardwareK9bot();
             robot.gyro.calibrate();
-
+//turning with gyro
             while (robot.gyro.getHeading() != heading) {
                 if (robot.gyro.getHeading() - heading > 180)
                     telemetry.addData("Turn robot right ");
